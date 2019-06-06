b851bd3944ab1afcc67bcfc8c144f0fb886d12e8
richard <richard.boon@nelen-schuurmans.nl>
2019-06-06 11:54:09 +0200

    Added validate_schema to schema.py

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -5,7 +5,7 @@ from threedi_modelchecker.schema import ModelSchema, constants
 def test_check_latest_migration_missing(threedi_db):
     factories.MigrationHistoryFactory()
     schema_checker = ModelSchema(threedi_db)
-    latest = schema_checker.is_latest_migration()
+    latest = schema_checker._is_latest_migration()
     assert not latest
 
 
@@ -15,5 +15,5 @@ def test_check_latest_migration_correct(threedi_db):
         migration=constants.LATEST_MIGRATION_NAME
     )
     schema_checker = ModelSchema(threedi_db)
-    latest = schema_checker.is_latest_migration()
+    latest = schema_checker._is_latest_migration()
     assert latest
--- threedi_modelchecker/schema.py
+++ threedi_modelchecker/schema.py
@@ -2,30 +2,47 @@ from sqlalchemy import func
 
 from .threedi_model import constants
 from .threedi_model import models
+from .errors import MigrationMissingError
+from .errors import MigrationTooHighError
 
 
 class ModelSchema:
     def __init__(self, threedi_db, declared_models=models.DECLARED_MODELS):
         self.db = threedi_db
         self.declared_models = declared_models
-        if not self.is_latest_migration():
-            print("WARNING: model is not the latest migration!")
+        self.validate_schema()
 
-    def is_latest_migration(self):
-        """Return if the database contains the latest migration"""
+    def _latest_migration(self):
+        """Returns a tuple with latest migration id and name"""
         session = self.db.get_session()
         latest_migration_id = session.query(
             func.max(models.SouthMigrationHistory.id)
         ).scalar()
         latest_migration_name = (
             session.query(models.SouthMigrationHistory.migration)
-            .filter(models.SouthMigrationHistory.id == latest_migration_id)
-            .scalar()
+                .filter(models.SouthMigrationHistory.id == latest_migration_id)
+                .scalar()
         )
-        return (
-            latest_migration_id == constants.LATEST_MIGRATION_ID
-            and latest_migration_name == constants.LATEST_MIGRATION_NAME
-        )
+        return latest_migration_id, latest_migration_name
+
+    def validate_schema(self):
+        """Very basic validation of 3Di schema.
+
+        Check that the database has the latest migration applied. If the
+        latest migrations is applied, we assume the database also contains all
+        tables and columns defined in threedi_model.models.py.
+
+        :return: True if the threedi_db schema is valid, raises an error otherwise.
+        :raise MigrationMissingError or MigrationTooHighError
+        """
+        migration_id, migration_name = self._latest_migration()
+        if migration_id < constants.LATEST_MIGRATION_ID:
+            raise MigrationMissingError
+        elif migration_id > constants.LATEST_MIGRATION_ID:
+            raise MigrationTooHighError
+        else:
+            return migration_id == constants.LATEST_MIGRATION_ID \
+                   and migration_name == constants.LATEST_MIGRATION_NAME
 
     def get_missing_tables(self):
         pass
