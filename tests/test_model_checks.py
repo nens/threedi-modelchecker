2b29384a3228849bf8e737b41a3e3b77fbe523ab
richard <richard.boon@nelen-schuurmans.nl>
2019-06-06 11:17:52 +0200

    Renamed and added documentation to some methods in model_checks

--- tests/test_model_checks.py
+++ tests/test_model_checks.py
@@ -9,11 +9,6 @@ def model_checker(threedi_db):
     return ThreediModelChecker(threedi_db)
 
 
-def test_get_model_errors(model_checker):
-    model_checker.get_model_errors()
-
-
 def test_get_model_error_iterator(model_checker):
-    error_iterator = model_checker.get_model_error_iterator()
-    for check, error in error_iterator:
+    for check, error in model_checker.errors():
         print(format_check_results(check, error))
