e3abaa9627d761dc3f0a5196bfc093a62e63e23c
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2021-09-30 11:02:02 +0200

    WIP

--- threedi_modelchecker/migrations/env.py
+++ threedi_modelchecker/migrations/env.py
@@ -1,6 +1,7 @@
 from alembic import context
 from threedi_modelchecker.threedi_model.models import Base
-
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
 
 target_metadata = Base.metadata
 config = context.config
@@ -13,6 +14,12 @@ def run_migrations_online():
     SQLite before running migrations.
     """
     connectable = config.attributes.get("connection")
+    if connectable is None:
+        connectable = engine_from_config(
+            config.get_section(config.config_ini_section),
+            prefix="sqlalchemy.",
+            poolclass=pool.NullPool,
+        )
 
     with connectable.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata)
