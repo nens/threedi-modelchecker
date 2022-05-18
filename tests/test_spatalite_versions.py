a03019e3cbdc0fc42e7e962d6ee514bab4d43b71
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2022-05-18 10:59:08 +0200

    WIP

--- tests/test_spatalite_versions.py
+++ tests/test_spatalite_versions.py
@@ -1,7 +1,9 @@
 from geoalchemy2 import func as geo_func
 from threedi_modelchecker.spatialite_versions import get_spatialite_version, copy_model
 from threedi_modelchecker.threedi_model import models
+from threedi_modelchecker.errors import UpgradeFailedError
 
+import pytest
 
 def test_get_spatialite_version(empty_sqlite_v3):
     lib_version, file_version = get_spatialite_version(empty_sqlite_v3)
@@ -32,7 +34,9 @@ def test_copy_model(empty_sqlite_v3, empty_sqlite_v3_clone):
         assert records == [(3, "test", "POINT(-71.064544 42.28787)", None)]
 
 
-def test_insert_invalid_geometry(empty_sqlite_v3, empty_sqlite_v3_clone):
+def test_copy_invalid_geometry(empty_sqlite_v3, empty_sqlite_v3_clone):
+    """Copying an invalid geometry (ST_IsValid evaluates to False) is possible
+    """
     obj = models.Surface(
         id=3, code="test", display_name="test", the_geom="SRID=4326;POLYGON((0 0, 10 10, 0 10, 10 0, 0 0))"
     )
@@ -51,3 +55,19 @@ def test_insert_invalid_geometry(empty_sqlite_v3, empty_sqlite_v3_clone):
         )
 
         assert records == [(3, "POLYGON((0 0, 10 10, 0 10, 10 0, 0 0))")]
+
+
+
+def test_copy_violates_null_constraint(empty_sqlite_v3, empty_sqlite_v3_clone):
+    """Copying an invalid geometry (ST_IsValid evaluates to False) is possible
+    """
+    obj = models.Manhole(
+        id=3, code="test", display_name="test"
+    )
+    with empty_sqlite_v3.session_scope() as session:
+        session.execute()
+        session.add(obj)
+        session.commit()
+
+    with pytest.raises(UpgradeFailedError):
+        copy_model(empty_sqlite_v3, empty_sqlite_v3_clone, models.Manhole)
--- threedi_modelchecker/errors.py
+++ threedi_modelchecker/errors.py
@@ -2,3 +2,9 @@ class MigrationMissingError(Exception):
     """Raised when 3Di model is missing migrations."""
 
     pass
+
+
+class UpgradeFailedError(Exception):
+    """Raised when an upgrade() fails"""
+
+    pass
