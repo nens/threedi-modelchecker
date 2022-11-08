82b0cf7b0b2be9aea89f32150a18573ad2727d76
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2022-11-08 17:03:17 +0100

    WIP Raster interface

---
+++ threedi_modelchecker/interfaces/raster_interface.py
@@ -0,0 +1,68 @@
+from functools import cached_property
+
+
+try:
+    from osgeo import gdal
+
+    gdal.UseExceptions()
+except ImportError:
+    gdal = None
+
+
+class RasterInterface:
+    def __init__(self, path):
+        self.path = str(path)
+
+    def __enter__(self):
+        try:
+            self._dataset = gdal.OpenEx(self.path, allowed_drivers=["gtiff"])
+        except RuntimeError:
+            self._dataset = None
+
+    def __exit__(self, *args, **kwargs):
+        self._dataset = None
+
+    @cached_property
+    def _spatial_reference(self):
+        return self._dataset.GetSpatialRef()
+
+    @cached_property
+    def _statistics(self):
+        return self._dataset.GetRasterBand(1).ComputeStatistics(False)
+
+    @property
+    def band_count(self):
+        return self._dataset.RasterCount
+
+    @property
+    def is_geographic(self):
+        sr = self._spatial_reference
+        if sr is None:
+            return False
+        else:
+            return bool(sr.IsGeographic())
+
+    @property
+    def epsg_code(self):
+        if not self.is_geographic:
+            return None
+        code = self._spatial_reference.GetAuthorityCode("PROJCS")
+        return int(code) if code is not None else None
+
+    @property
+    def pixel_size(self):
+        gt = self._dataset.GetGeoTransform()
+        if gt is None:
+            return None, None
+        else:
+            return abs(gt[1]), abs(gt[5])
+
+    def min(self):
+        if self.band_count == 0:
+            return None
+        return self._statistics[0]
+
+    def max(self):
+        if self.band_count == 0:
+            return None
+        return self._statistics[1]
