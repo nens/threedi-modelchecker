40bcd571c3cfa2de35a2488f300b591586c3991b
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2022-11-09 11:22:32 +0100

    Refactor tests

--- tests/test_checks_raster.py
+++ tests/test_checks_raster.py
@@ -1,54 +1,98 @@
 from tests import factories
+from threedi_modelchecker.checks.raster import BaseRasterCheck
 from threedi_modelchecker.checks.raster import LocalContext
 from threedi_modelchecker.checks.raster import RasterExistsCheck
 from threedi_modelchecker.checks.raster import ServerContext
 from threedi_modelchecker.threedi_model import models
+from unittest import mock
+
+import pytest
 
 
-def test_exists_check_filesystem_ok(session, tmp_path):
-    (tmp_path / "somefile").touch()
+@pytest.fixture
+def mocked_check():
+    with mock.patch.object(BaseRasterCheck, "is_valid_local", return_value=True):
+        with mock.patch.object(BaseRasterCheck, "is_valid_server", return_value=True):
+            yield BaseRasterCheck(column=models.GlobalSetting.dem_file)
+
+
+@pytest.fixture
+def context_local(tmp_path):
+    return LocalContext(base_path=tmp_path)
+
+
+@pytest.fixture
+def context_server():
+    return ServerContext(available_rasters={})
+
+
+@pytest.fixture
+def session_local(session, context_local):
+    session.model_checker_context = context_local
+    return session
+
+
+@pytest.fixture
+def session_server(session, context_server):
+    session.model_checker_context = context_server
+    return session
+
+
+def test_base_to_check(session):
     factories.GlobalSettingsFactory(dem_file="somefile")
-    session.model_checker_context = LocalContext(base_path=tmp_path)
-    check = RasterExistsCheck(column=models.GlobalSetting.dem_file)
-    invalid_rows = check.get_invalid(session)
-    assert len(invalid_rows) == 0
+    check = BaseRasterCheck(column=models.GlobalSetting.dem_file)
+    assert check.to_check(session).count() == 1
 
 
-def test_exists_check_filesystem_ignore(session, tmp_path):
+def test_base_to_check_ignores_empty(session):
     factories.GlobalSettingsFactory(dem_file="")
-    session.model_checker_context = LocalContext(base_path=tmp_path)
-    check = RasterExistsCheck(column=models.GlobalSetting.dem_file)
-    invalid_rows = check.get_invalid(session)
-    assert len(invalid_rows) == 0
+    check = BaseRasterCheck(column=models.GlobalSetting.dem_file)
+    assert check.to_check(session).count() == 0
 
 
-def test_exists_check_filesystem_err(session, tmp_path):
-    factories.GlobalSettingsFactory(dem_file="some/file")
-    session.model_checker_context = LocalContext(base_path=tmp_path)
-    check = RasterExistsCheck(column=models.GlobalSetting.dem_file)
-    invalid_rows = check.get_invalid(session)
-    assert len(invalid_rows) == 1
+def test_base_to_check_ignores_none(session):
+    factories.GlobalSettingsFactory(dem_file=None)
+    check = BaseRasterCheck(column=models.GlobalSetting.dem_file)
+    assert check.to_check(session).count() == 0
 
 
-def test_exists_check_context_ok(session):
-    factories.GlobalSettingsFactory(dem_file="some/file")
-    session.model_checker_context = ServerContext(available_rasters={"dem_file"})
-    check = RasterExistsCheck(column=models.GlobalSetting.dem_file)
-    invalid_rows = check.get_invalid(session)
-    assert len(invalid_rows) == 0
+def test_base_get_invalid_local(mocked_check, session_local):
+    factories.GlobalSettingsFactory(dem_file="somefile")
+    assert mocked_check.get_invalid(session_local) == []
+    mocked_check.is_valid_local.assert_called_once_with(
+        "somefile", session_local.model_checker_context
+    )
+    assert not mocked_check.is_valid_server.called
 
 
-def test_exists_check_context_err(session):
-    factories.GlobalSettingsFactory(dem_file="some/file")
-    session.model_checker_context = ServerContext(available_rasters={"frict_coef_file"})
-    check = RasterExistsCheck(column=models.GlobalSetting.dem_file)
-    invalid_rows = check.get_invalid(session)
-    assert len(invalid_rows) == 1
+def test_base_get_invalid_server(mocked_check, session_server):
+    factories.GlobalSettingsFactory(dem_file="somefile")
+    assert mocked_check.get_invalid(session_server) == []
+    mocked_check.is_valid_server.assert_called_once_with(
+        "somefile", session_server.model_checker_context
+    )
+    assert not mocked_check.is_valid_local.called
 
 
-def test_exists_check_context_ignore(session):
-    factories.GlobalSettingsFactory(dem_file="")
-    session.model_checker_context = ServerContext(available_rasters={"frict_coef_file"})
-    check = RasterExistsCheck(column=models.GlobalSetting.dem_file)
-    invalid_rows = check.get_invalid(session)
-    assert len(invalid_rows) == 0
+@pytest.fixture
+def exists_check():
+    return RasterExistsCheck(column=models.GlobalSetting.dem_file)
+
+
+def test_exists_is_valid_local_ok(context_local, tmp_path, exists_check):
+    (tmp_path / "somefile").touch()
+    assert exists_check.is_valid_local("somefile", context_local)
+
+
+def test_exists_is_valid_local_err(context_local, exists_check):
+    assert not exists_check.is_valid_local("somefile", context_local)
+
+
+def test_exists_is_valid_server_ok(context_server, exists_check):
+    context_server.available_rasters = {"dem_file"}
+    assert exists_check.is_valid_server("somefile", context_server)
+
+
+def test_exists_is_valid_server_err(context_server, exists_check):
+    context_server.available_rasters = {"other"}
+    assert not exists_check.is_valid_server("somefile", context_server)
