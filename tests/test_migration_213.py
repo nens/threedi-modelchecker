774fd396564fed55a6a656e875a5620b5a13e4e2
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2022-12-21 20:42:58 +0100

    WIP

--- tests/test_migration_213.py
+++ tests/test_migration_213.py
@@ -2,11 +2,25 @@ from importlib.machinery import SourceFileLoader
 from pathlib import Path
 from threedi_modelchecker import ThreediDatabase
 from threedi_modelchecker.schema import ModelSchema
-from threedi_modelchecker.threedi_model.models import CalculationPoint
-from threedi_modelchecker.threedi_model.models import ConnectedPoint
 
 import pytest
 import threedi_modelchecker
+
+
+path = (
+    Path(threedi_modelchecker.__file__).parent
+    / "migrations"
+    / "versions"
+    / "0213_connected_points_to_breaches.py"
+)
+migration_213 = SourceFileLoader("migration_213", str(path)).load_module(
+    "migration_213"
+)
+
+CalculationPoint = migration_213.CalculationPoint
+ConnectedPoint = migration_213.ConnectedPoint
+Levee = migration_213.Levee
+PotentialBreach = migration_213.PotentialBreach
 
 
 @pytest.fixture
@@ -18,24 +32,13 @@ def in_memory_sqlite():
 
 
 @pytest.fixture
-def connection(in_memory_sqlite):
-    with in_memory_sqlite.get_engine().connect() as connection:
-        yield connection
-
-
-@pytest.fixture
-def migration_213():
-    path = (
-        Path(threedi_modelchecker.__file__).parent
-        / "migrations"
-        / "versions"
-        / "0213_connected_points_to_breaches.py"
-    )
-    return SourceFileLoader("migration_213", str(path)).load_module("migration_213")
+def session(in_memory_sqlite):
+    return in_memory_sqlite.get_session()
 
 
 GEOM1 = "SRID=4326;POINT (0 0)"
 GEOM2 = "SRID=4326;POINT (0 1)"
+LINE = "SRID=4326;LINESTRING(0 0,0 1)"
 
 
 @pytest.mark.parametrize(
@@ -55,11 +58,10 @@ GEOM2 = "SRID=4326;POINT (0 1)"
         ],
     ],
 )
-def test_clean_connected_points(in_memory_sqlite, connection, migration_213, objs):
-    session = in_memory_sqlite.get_session()
+def test_clean_connected_points(session, objs):
     session.add_all(objs)
     session.flush()
-    migration_213.clean_connected_points(connection)
+    migration_213.clean_connected_points(session)
     assert session.query(CalculationPoint).count() == 0
     assert session.query(ConnectedPoint).count() == 0
 
@@ -78,13 +80,79 @@ def test_clean_connected_points(in_memory_sqlite, connection, migration_213, obj
         ],
     ],
 )
-def test_clean_connected_points_keep(in_memory_sqlite, connection, migration_213, objs):
-    session = in_memory_sqlite.get_session()
+def test_clean_connected_points_keep(session, objs):
     session.add_all(objs)
     session.flush()
-    migration_213.clean_connected_points(connection)
+    migration_213.clean_connected_points(session)
 
     actual = (
         session.query(CalculationPoint).count() + session.query(ConnectedPoint).count()
     )
     assert actual == len(objs)
+
+
+def todict(x):
+    return {col.name: getattr(x, col.name) for col in x.__table__.columns}
+
+
+def assert_sqlalchemy_objects_equal(a, b):
+    assert a.__class__ is b.__class__
+    assert todict(a) == todict(b)
+
+
+@pytest.mark.parametrize(
+    "objs,expected",
+    [
+        [
+            [
+                CalculationPoint(id=1, the_geom=GEOM1, user_ref="123#4#v2_channel#4"),
+                ConnectedPoint(id=1, the_geom=GEOM2, calculation_pnt_id=1),
+            ],
+            PotentialBreach(id=1, channel_id=4, the_geom=LINE),
+        ],
+        [
+            [
+                CalculationPoint(id=1, the_geom=GEOM1, user_ref="123#4#v2_channel#4"),
+                ConnectedPoint(
+                    id=1, the_geom=GEOM2, calculation_pnt_id=1, exchange_level=1.1
+                ),
+            ],
+            PotentialBreach(id=1, channel_id=4, the_geom=LINE, exchange_level=1.1),
+        ],
+        [
+            [
+                CalculationPoint(id=1, the_geom=GEOM1, user_ref="123#4#v2_channel#4"),
+                ConnectedPoint(id=1, the_geom=GEOM2, calculation_pnt_id=1, levee_id=4),
+                Levee(id=4, crest_level=1.1),
+            ],
+            PotentialBreach(id=1, channel_id=4, the_geom=LINE, exchange_level=1.1),
+        ],
+        [
+            [
+                CalculationPoint(id=1, the_geom=GEOM1, user_ref="123#4#v2_channel#4"),
+                ConnectedPoint(
+                    id=1,
+                    the_geom=GEOM2,
+                    calculation_pnt_id=1,
+                    exchange_level=1.1,
+                    levee_id=4,
+                ),
+                Levee(id=4, crest_level=1.2, max_breach_depth=0.5, material=1),
+            ],
+            PotentialBreach(
+                id=1,
+                channel_id=4,
+                the_geom=LINE,
+                exchange_level=1.1,
+                maximum_breach_depth=0.5,
+                levee_material=1,
+            ),
+        ],
+    ],
+)
+def test_transform(session, objs, expected):
+    session.add_all(objs)
+    session.flush()
+    actual = migration_213.transform(session, 1)
+
+    assert_sqlalchemy_objects_equal(actual, expected)
--- threedi_modelchecker/migrations/versions/0213_connected_points_to_breaches.py
+++ threedi_modelchecker/migrations/versions/0213_connected_points_to_breaches.py
@@ -6,6 +6,14 @@ Create Date: 2022-12-21 14:54:00
 
 """
 from alembic import op
+from geoalchemy2.types import Geometry
+from sqlalchemy import Column
+from sqlalchemy import Float
+from sqlalchemy import ForeignKey
+from sqlalchemy import func
+from sqlalchemy import Integer
+from sqlalchemy import orm
+from sqlalchemy import String
 
 
 # revision identifiers, used by Alembic.
@@ -15,34 +23,153 @@ branch_labels = None
 depends_on = None
 
 
-DROP_NON_DISPLACED = """
-DELETE FROM v2_connected_pnt WHERE v2_connected_pnt.calculation_pnt_id IS NULL;
-DELETE FROM v2_connected_pnt
-  WHERE NOT EXISTS
-    (SELECT 1 FROM v2_calculation_point
-     WHERE v2_calculation_point.id = v2_connected_pnt.calculation_pnt_id);
-DELETE FROM v2_connected_pnt
-  WHERE v2_connected_pnt.id IN (
-    SELECT v2_connected_pnt.id FROM v2_connected_pnt
-    JOIN v2_calculation_point ON v2_connected_pnt.calculation_pnt_id = v2_calculation_point.id
-    WHERE v2_connected_pnt.the_geom = v2_calculation_point.the_geom
-  );
-DELETE FROM v2_calculation_point
-  WHERE NOT EXISTS
-    (SELECT 1 FROM v2_connected_pnt
-     WHERE v2_connected_pnt.calculation_pnt_id = v2_calculation_point.id);
-"""
+## Copy of the ORM at this point in time:
+
+Base = orm.declarative_base()
 
 
-def clean_connected_points(connection):
-    for q in DROP_NON_DISPLACED.split(";"):
-        connection.execute(q)
+class Levee(Base):
+    __tablename__ = "v2_levee"
+    id = Column(Integer, primary_key=True)
+    code = Column(String(100))
+    crest_level = Column(Float)
+    the_geom = Column(
+        Geometry(
+            geometry_type="LINESTRING", srid=4326, spatial_index=True, management=True
+        ),
+        nullable=False,
+    )
+    material = Column(Integer)
+    max_breach_depth = Column(Float)
+
+
+class CalculationPoint(Base):
+    __tablename__ = "v2_calculation_point"
+    id = Column(Integer, primary_key=True)
+
+    content_type_id = Column(Integer)
+    user_ref = Column(String(80), nullable=False)
+    calc_type = Column(Integer)
+    the_geom = Column(
+        Geometry(geometry_type="POINT", srid=4326),
+        nullable=False,
+    )
+
+
+class ConnectedPoint(Base):
+    __tablename__ = "v2_connected_pnt"
+    id = Column(Integer, primary_key=True)
+
+    calculation_pnt_id = Column(
+        Integer, ForeignKey(CalculationPoint.__tablename__ + ".id"), nullable=False
+    )
+    levee_id = Column(Integer, ForeignKey(Levee.__tablename__ + ".id"))
+
+    exchange_level = Column(Float)
+    the_geom = Column(
+        Geometry(geometry_type="POINT", srid=4326),
+        nullable=False,
+    )
+
+
+class PotentialBreach(Base):
+    __tablename__ = "v2_potential_breach"
+    id = Column(Integer, primary_key=True)
+    code = Column(String(100))
+    display_name = Column(String(255))
+    exchange_level = Column(Float)
+    maximum_breach_depth = Column(Float)
+    levee_material = Column(Integer)
+    the_geom = Column(
+        Geometry(geometry_type="LINESTRING", srid=4326),
+        nullable=False,
+    )
+    channel_id = Column(Integer, nullable=False)
+
+
+def parse_connected_point_user_ref(user_ref: str):
+    """Return content_type, content_id, node_number from a user_ref.
+
+    Raises Exception for various parse errors.
+
+    Example
+    -------
+    >>> parse_connected_point_user_ref("201#123#v2_channels#4)
+    ContentType.TYPE_V2_CHANNEL, 123, 4
+    """
+    _, id_str, type_str, _ = user_ref.split("#")
+    return type_str, int(id_str)
+
+
+def clean_connected_points(session):
+    conn_point_ids = [
+        x[0]
+        for x in session.query(ConnectedPoint.id)
+        .join(CalculationPoint, isouter=True)
+        .filter(ConnectedPoint.the_geom != CalculationPoint.the_geom)
+        .all()
+    ]
+    session.query(ConnectedPoint).filter(
+        ConnectedPoint.id.notin_(conn_point_ids)
+    ).delete(synchronize_session="fetch")
+    calc_point_ids = [
+        x[0]
+        for x in session.query(ConnectedPoint.calculation_pnt_id)
+        .filter(ConnectedPoint.id.in_(conn_point_ids))
+        .all()
+    ]
+    session.query(CalculationPoint).filter(
+        CalculationPoint.id.notin_(calc_point_ids)
+    ).delete(synchronize_session="fetch")
+    return conn_point_ids
+
+
+def transform(session, conn_point_id):
+    connected_point, calculation_point, levee, line_geom = (
+        session.query(
+            ConnectedPoint,
+            CalculationPoint,
+            Levee,
+            func.AsEWKT(
+                func.MakeLine(CalculationPoint.the_geom, ConnectedPoint.the_geom)
+            ),
+        )
+        .join(CalculationPoint)
+        .join(Levee, isouter=True)
+        .filter(ConnectedPoint.id == conn_point_id)
+        .first()
+    )
+
+    try:
+        type_ref, pk = parse_connected_point_user_ref(calculation_point.user_ref)
+    except Exception:
+        return
+
+    if connected_point.exchange_level is not None:
+        exchange_level = connected_point.exchange_level
+    elif levee is not None:
+        exchange_level = levee.crest_level
+    else:
+        exchange_level = None
+
+    if type_ref == "v2_channel":
+        channel_id = pk
+    else:
+        raise NotImplementedError()
+    return PotentialBreach(
+        id=connected_point.id,
+        exchange_level=exchange_level,
+        maximum_breach_depth=levee.max_breach_depth if levee is not None else None,
+        levee_material=levee.material if levee is not None else None,
+        the_geom=line_geom,
+        channel_id=channel_id,
+    )
 
 
 def upgrade():
-    conn = op.get_bind()
+    session = orm.Session(bind=op.get_bind())
 
-    clean_connected_points(conn)
+    clean_connected_points(session)
 
 
 def downgrade():
