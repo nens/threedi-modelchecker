e781cf6b4ce6eec3406098f5d1375e4e68eadb66
elisallenens <eli.salle@nelen-schuurmans.nl>
2023-02-06 10:58:22 +0100

    fix linter issue

--- threedi_modelchecker/config.py
+++ threedi_modelchecker/config.py
@@ -587,7 +587,8 @@ CHECKS += [
         invalid=Query(table)
         .join(
             models.ConnectionNode,
-            table.connection_node_start_id == models.ConnectionNode.id or table.connection_node_end_id == models.ConnectionNode.id,
+            table.connection_node_start_id == models.ConnectionNode.id
+            or table.connection_node_end_id == models.ConnectionNode.id,
         )
         .join(models.Manhole)
         .filter(
