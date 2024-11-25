cc3a0410c9611419e9ef6bcef32252302013658c
elisalle <eli.salle@nelen-schuurmans.nl>
2024-11-25 14:49:25 +0100

    use AssertionError instead of ValueError; do not order checks in test

--- threedi_modelchecker/tests/test_error_messages.py
+++ threedi_modelchecker/tests/test_error_messages.py
@@ -6,19 +6,17 @@ checks = Config(models=DECLARED_MODELS).checks
 
 
 def test_error_description():
-    ordered_checks = sorted(checks, key=lambda k: (k.error_code, k.description()))
-
-    for check in ordered_checks:
+    for check in checks:
         check_code = str(check.error_code).zfill(4)
         try:
             check.level.name.capitalize()
         except Exception as e:
-            raise ValueError(
+            raise AssertionError(
                 f"Could not capitalise level name for check {check_code}"
             ) from e
         try:
             check.description()
         except Exception as e:
-            raise ValueError(
+            raise AssertionError(
                 f"Could not generate description for check {check_code}"
             ) from e
