e218e3b39e6a1e8cc8b04e232cee108f2c2c48f7
elisalle <eli.salle@nelen-schuurmans.nl>
2025-08-21 15:50:37 +0200

    run black

--- threedi_modelchecker/config.py
+++ threedi_modelchecker/config.py
@@ -1024,7 +1024,7 @@ CHECKS += [
         column=models.ConnectionNode.exchange_level,
         filters=or_(
             CONDITIONS["has_dem"].exists(),
-            models.ModelSettings.manhole_aboveground_storage_area > 0
+            models.ModelSettings.manhole_aboveground_storage_area > 0,
         ),
         invalid=Query(models.ConnectionNode).filter(
             models.ConnectionNode.exchange_type.in_(
